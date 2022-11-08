d6c88cf3ac4d9e708b9810a486eb8ade027197fb
jasonkey19 <colin030313@gmail.com>
2022-11-08 12:15:51 +0800

    修改目錄

---
+++ week5/week 05-3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    ///     int a=3;
+    ///    while(a>0){
+    ///    printf("a是%d,a>0\n",a);
+
+    ///    a--///課本第二章,運算
+    ///    }開始的值 中間斷尾  收尾
+    for    (int a=3;   a>0;      a--)
+    {
+            printf("a是%d,a>0\n",a);
+    }
+}
