237d287cebece0fc9aa557c1bfc5a1c7b293169c
jasonkey19 <colin030313@gmail.com>
2022-12-20 11:59:24 +0800

    week16

---
+++ week 16/week 16-1.c
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+int main()
+{
+    printf("請輸入數字`一個字母\n");
+    int a;
+    scanf("%d",&a);
+
+    char c;
+    scanf("%c",&c);
+
+    printf("數字是:%d字母是:%c",a,c);
+
+
+}
---
+++ week 16/week 16-3.cpp
@@ -0,0 +1,16 @@
+#include <Stdio.h>
+int main()
+{
+    int a=10;
+    int *p=&a;
+
+    float f=3.14145926;
+    float *p2=&f;
+
+    char c='A';
+    char *p3=&c;
+
+    char line [10]="Hello";
+    char *p4=line;
+
+}
